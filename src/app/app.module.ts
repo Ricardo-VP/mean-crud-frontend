271ba342690747c539cfa07316a6a81b2f77bcf7
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-28 22:15:15 -0500

    Agregando la estructura de nuestra app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
